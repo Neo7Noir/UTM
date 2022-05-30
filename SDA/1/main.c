dac14dbf6544ce16c5f978b296a362182eb54bfc
unknown <vladimir.vozian777@gmail.com>
2022-05-30 07:39:21 +0300

    CSA

---
+++ SDA/1/main.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "program.c"
+#include "header.h"
+
+void menu(){
+	printf("1) Adaugarea elementrlor in lista.\n");
+	printf("2) Cautarea nodului in arbore.\n");
+	printf("3) Modificarea informatiei unui nod din arbore.\n");
+	printf("4) Determinarea numarului de noduri.\n");
+	printf("5) Determinarea inaltimii arborelui.\n");
+	printf("6) Eliberarea memoriei alocata pentru arbore.\n");
+	printf("0) Iesirea din program.\n");
+}
+
+int main(){
+	struct tree *head = NULL;
+
+	int num, option;
+	char str[30], str2[30];
+
+	srand(time(NULL));
+
+	system("cls");
+	printf("Dati numarul de canale:");
+	scanf("%d",&num);
+
+	citire(&head,num);
+
+	do{
+		system("cls");
+		printIn(head);
+		printf("\n");
+		menu();
+		printf("Alegeti optiunea:");
+		scanf("%d",&option);
+
+		switch(option){
+			case 1:
+				printf("Dati numarul de capitale:");
+				scanf("%d",&num);
+
+				citire(&head,num);
+			break;
+
+			case 2:
+				printf("Dati denumirea capitalei ce va intereseaza:");
+				scanf("%s",str);
+
+				struct tree *temp = search(head, str);
+
+				printIn(temp);
+			break;
+
+			case 3:
+				printf("Dati denumirea capitalei ce doriti sa modificati:");
+				scanf("%s",str);
+
+				change(&head, str);
+			break;
+
+			case 4:
+				numNode(head);
+				printf("Arborele are %d noduri.", count);
+				count = 0;
+			break;
+
+			case 5:
+				printf("Inaltimea arborelui este: %d.",heightOfTree(head));
+			break;
+
+			case 6:
+				clearTree(&head);
+			break;
+
+			case 0: printf("By V.P.\n Thanks");
+			break;
+		}
+
+		printf("\nPress any key to continue...");
+		getch();
+	}while(option != 0);
+	return 0;
+}
